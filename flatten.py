deef885238932c6c40e966de0f01c78accf61036
Stelios Vitalis <steliosvitalis@gmail.com>
2018-12-13 15:38:36 +0100

    Make plane calculation work

--- flatten.py
+++ flatten.py
@@ -10,4 +10,20 @@ A = np.vstack([x, y, np.ones(len(x))]).T
 
 n = np.linalg.lstsq(A, z, rcond=None)[0]
 
-print(n)
+# n0 * x0 + n1 * y0 + n2 * z0 = d
+
+print("This is the A matrix:")
+print(A)
+
+n = n / np.sqrt((n ** 2).sum(-1))
+a, b, c = n
+d = a * x[0] + b * y[0] + c * z[0]
+
+print("This is the normal and the d coefficient")
+print(a, b, c, d)
+
+for i in range(1, 4):
+    s = a * x[i] + b * y[i] + c * z[i] - d
+
+    print("This is the 'distance' of point ", i)
+    print(s)
