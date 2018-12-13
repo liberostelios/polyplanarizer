8f7a4025b11c34fd517394caab05dd28fd385c5c
Stelios Vitalis <steliosvitalis@gmail.com>
2018-12-13 16:40:28 +0100

    Last version of least squares

--- flatten.py
+++ flatten.py
@@ -2,18 +2,24 @@ import numpy as np
 
 coord = np.loadtxt("Dachlinien.csv", delimiter=",", skiprows=1)
 
-x = coord[:,1]
-y = coord[:,2]
-z = coord[:,3]
+x = coord[:-1,1]
+y = coord[:-1,2]
+z = coord[:-1,3]
 
 A = np.vstack([x, y, np.ones(len(x))]).T
 
-n = np.linalg.lstsq(A, z, rcond=None)[0]
+result = np.linalg.lstsq(A, z, rcond=None)
 
-# n0 * x0 + n1 * y0 + n2 * z0 = d
+n = result[0]
 
 print("This is the A matrix:")
 print(A)
+
+print("Those are the z values:")
+print(z)
+
+print("This is the result:")
+print(result)
 
 n = n / np.sqrt((n ** 2).sum(-1))
 a, b, c = n
@@ -22,8 +28,15 @@ d = a * x[0] + b * y[0] + c * z[0]
 print("This is the normal and the d coefficient")
 print(a, b, c, d)
 
-for i in range(1, 4):
-    s = a * x[i] + b * y[i] + c * z[i] - d
+zp = np.zeros(len(x))
 
-    print("This is the 'distance' of point ", i)
-    print(s)
+for i in range(0, 4):
+    # Calculate the sigma value - sort of an error
+    s = a * x[i] + b * y[i] + c * z[i] - d
+    print("The sigma of point ", i, " is: ", s)
+    
+    # Calculate the z' according to the plane
+    zp[i] = - (a * x[i] + b * y[i] - d) / c
+    print("New point is: ", x[i], y[i], zp[i], " (old Z was: ", z[i], ")")
+
+np.savetxt("new_points.csv", np.array([x, y, zp]).T, fmt="%.11f", delimiter=",")
